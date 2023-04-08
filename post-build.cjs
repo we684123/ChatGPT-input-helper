8e388d76d988baa17d86a87f5fb69ef929766c4c
we684123 <we684123@gmail.com>
2023-04-09 01:10:01 +0800

    🏗️ 把 tampermonkeyHeader 移到 src 下面

--- post-build.cjs
+++ post-build.cjs
@@ -1,25 +1,8 @@
 const fs = require('fs');
 const path = require('path');
+const tampermonkeyHeader = fs.readFileSync('./src/config.tampermonkeyHeader', 'utf8');
 
 const outputPath = path.resolve(__dirname, 'dist', 'bundle.js');
-const tampermonkeyHeader = `// ==UserScript==
-// @name         ChatGPT-input-helper2
-// @name:zh-TW   ChatGPT-input-helper2 快速輸入常用咒文
-// @namespace    https://github.com/we684123/ChatGPT-input-helper2
-// @version      0.0.4
-// @author       we684123
-// @description  Help organize commonly used spells quickly
-// @description:zh-TW  幫助快速組織常用咒文
-// @license      MIT
-// @icon         https://chat.openai.com/favicon.ico
-// @match        https://chat.openai.com/chat
-// @match        https://chat.openai.com/chat/*
-// @grant        GM_getValue
-// @grant        GM_setValue
-// @grant        GM_deleteValue
-// @run-at       document-end
-// ==/UserScript==
-`;
 
 fs.readFile(outputPath, 'utf8', (err, data) => {
   if (err) {
@@ -27,7 +10,7 @@ fs.readFile(outputPath, 'utf8', (err, data) => {
     return;
   }
 
-  const modifiedContent = tampermonkeyHeader + data;
+  const modifiedContent = tampermonkeyHeader + "\n\n" + data;
   fs.writeFile(outputPath, modifiedContent, 'utf8', (writeErr) => {
     if (writeErr) {
       console.error('寫入檔案失敗:', writeErr);
