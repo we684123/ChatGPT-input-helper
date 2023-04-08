59c08551b98d512c56e217a5b49a38f2a8c67a12
we684123 <we684123@gmail.com>
2023-04-08 20:31:52 +0800

    🍻

--- src/library.ts
+++ src/library.ts
@@ -7,3 +7,9 @@ export function addElementToPage(selector: string, element: HTMLElement): void {
     console.error("目標元素未找到，請確認選擇器是否正確");
   }
 }
+
+export function createHelperMenuContainer(): HTMLElement {
+  const container = document.createElement("div");
+  container.classList.add("nav-aims-container");
+  return container;
+}
