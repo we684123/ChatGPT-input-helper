a8b1f71856a3b0130f07dca7e3bf5c4bcdd786fe
we684123 <we684123@gmail.com>
2023-04-09 02:00:23 +0800

    🏗️ 將 config.ts 獨立出來

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import sentinel from "./utils/sentinel";
 import { onloadSafe } from "./utils/utils";
+
+import { config } from "./config";
 import "./style.css";
 
 main();
@@ -10,15 +12,43 @@ function main() {
     // 監聽 nav 元素
     console.log("=====監聽 nav 元素=====");
 
+    // 定義常用咒文
+    let customize: any;
+
     sentinel.on("nav", (nav: any) => {
-      console.log("===== sentinel.on nav 1111=====");
-      console.log("nav", nav);
-      console.log("nav11111");
-    });
-    sentinel.on("nav", (nav: any) => {
-      console.log("===== sentinel.on nav 2222=====");
-      console.log("nav", nav);
-      console.log("nav22222");
+      console.log("===== trigger sentinel.on nav =====");
+
+      const container = document.getElementById("helper_menu");
+      let GM_customize = GM_getValue("customizeData", customize); // 讀取 customize 設定
+      if (GM_customize) {
+        customize = GM_customize;
+      } else {
+        customize = config.init_customize;
+        GM_setValue("customizeData", customize);
+      }
+
+      //找不到就新增
+      if (!container) {
+        // 獲得目標元素
+        const aimsNode = document.querySelector(config.NAV_MENU) as HTMLElement;
+        // 新增一個容器
+        const container = document.createElement("div");
+        container.classList.add(config.CONTAINER_CLASS);
+        container.id = "helper_menu";
+
+        if (aimsNode) {
+          // 設定 container 寬度為父元素寬度
+          container.style.width = `${aimsNode.offsetWidth}px`; // 設定 container 寬度為父元素寬度
+
+          // 將容器元素插入到目標元素後面
+          aimsNode.parentNode?.insertBefore(container, aimsNode.nextSibling);
+
+          // 新增一個按鈕元素
+          addMenuBtn(container, customize, config.HELPER_MENU_TEXT);
+
+          showPopup(customize);
+        }
+      }
     });
   });
 }
