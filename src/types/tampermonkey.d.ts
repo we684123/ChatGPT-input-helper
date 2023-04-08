f22fe72d106b49d9925cd36640216f49c98bffde
we684123 <we684123@gmail.com>
2023-04-09 01:34:46 +0800

    ✨🚨 tampermonkey.d.ts

---
+++ src/types/tampermonkey.d.ts
@@ -0,0 +1,2 @@
+declare const GM_getValue: (name: string, defaultValue?: string | number | boolean | null) => string | number | boolean | null;
+declare const GM_setValue: (name: string, value: string | number | boolean | null) => void;
