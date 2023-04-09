d3c44a69802fdb292c7210afb02a2097ca12bc30
we684123 <we684123@gmail.com>
2023-04-09 16:16:11 +0800

    🐛 修正 無法自動送出文字 的問題

--- src/libs/insertCustomize.ts
+++ src/libs/insertCustomize.ts
@@ -4,7 +4,6 @@ import { Customization } from "../types/index";
 // 將自定義內容插入到輸入框中
 export const insertCustomize = (customize: Customization[], name: string) => {
     const textInputbox = document.querySelector(config.TEXT_INPUTBOX_POSITION) as HTMLTextAreaElement;
-    const submitButton = document.querySelector(config.SUBMIT_BUTTON_POSITION) as HTMLButtonElement;
     const item = customize.find((i: any) => i.name === name);
 
     if (item) {
@@ -18,7 +17,10 @@ export const insertCustomize = (customize: Customization[], name: string) => {
         textInputbox.focus();
 
         if (item.autoEnter) {
-            submitButton.click();
+            setTimeout(() => {
+                const submitButton = document.querySelector(config.SUBMIT_BUTTON_POSITION) as HTMLButtonElement;
+                submitButton.click();
+            }, 100);
         }
     } else {
         console.error(`找不到名稱為 ${name} 的元素`);
