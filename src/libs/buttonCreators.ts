b3fec8864dd98ecef8af05ec188184449add8ced
we684123 <we684123@gmail.com>
2023-04-09 13:42:29 +0800

    💡 addMenuBtn.ts , buttonCreators.ts

--- src/libs/buttonCreators.ts
+++ src/libs/buttonCreators.ts
@@ -1,6 +1,7 @@
 import { config } from "../config";
 import { Customization } from "../types/index";
 
+// 創建主按鈕
 export const createMainButton = (buttonText: string): HTMLButtonElement => {
     const mainButton = document.createElement("button");
     mainButton.innerText = buttonText;
@@ -9,6 +10,7 @@ export const createMainButton = (buttonText: string): HTMLButtonElement => {
     return mainButton;
 };
 
+// 創建設定按鈕
 export const createSettingButton = (): HTMLButtonElement => {
     const settingButton = document.createElement("button");
     settingButton.innerText = "⚙️";
@@ -18,6 +20,7 @@ export const createSettingButton = (): HTMLButtonElement => {
     return settingButton;
 };
 
+// 創建按鈕容器，用於存放主按鈕和設定按鈕
 export const createButtonContainer = (
     mainButton: HTMLButtonElement,
     settingButton: HTMLButtonElement
@@ -30,65 +33,49 @@ export const createButtonContainer = (
     return buttonContainer;
 };
 
+// 將自定義內容插入到輸入框中
 export const insertCustomize = (customize: any, name: string) => {
-    // customize = 設定的object
-    // name = 觸發按鈕的名稱
-
-    console.log('insertCustomize 開始執行');
     const textInputbox = document.querySelector(config.TEXT_INPUTBOX_POSITION) as HTMLTextAreaElement;
     const submitButton = document.querySelector(config.SUBMIT_BUTTON_POSITION) as HTMLButtonElement;
     const item = customize.find((i: any) => i.name === name);
 
     if (item) {
-        console.log(`已找到名稱為 ${name} 的元素`);
-
-        // start = 最前面 , end = 最後面
-        // 有想要出游標定位，之後再說
-        // 例如 `請從 "" 繼續輸出` 就希望定在 "" 之間
         if (item.position === 'start') {
             textInputbox.value = item.content + textInputbox.value;
         } else {
             textInputbox.value += item.content;
         }
 
-        // 模擬，讓對話框可以自動更新高度
         textInputbox.dispatchEvent(config.INPUT_EVENT);
         textInputbox.focus();
 
-        console.log(`textInputbox.value =\n${textInputbox.value}`);
-
-        // 如果需要，自動按下 Enter
         if (item.autoEnter) {
-            console.log('autoEnter (O)');
             submitButton.click();
         }
-
-        console.log('====== insertCustomize 結束 ======');
     } else {
         console.error(`找不到名稱為 ${name} 的元素`);
     }
 };
 
-
+// 創建選單項目
 const createMenuItem = (element: any, customize: Customization[]) => {
     const menuItem = document.createElement("button");
     menuItem.innerText = element.name;
     menuItem.id = element.name;
     menuItem.addEventListener("click", (event) => {
-        console.log("按鈕資訊：", event.target);
         insertCustomize(customize, (event.target as HTMLElement).id);
     });
 
     return menuItem;
 };
 
-
+// 創建選單，包含多個選單項目
 export const createMenu = (containerNode: any, customize: Customization[]) => {
-    const menu = document.createElement('div');
-    menu.id = 'helper_menu';
+    const menu = document.createElement("div");
+    menu.id = "helper_menu";
     menu.classList.add(config.MENU_CLASS);
-    menu.style.display = 'none';
-    menu.style.width = `${containerNode.offsetWidth}px`
+    menu.style.display = "none";
+    menu.style.width = `${containerNode.offsetWidth}px`;
 
     customize.forEach((element: any) => {
         const menuItem = createMenuItem(element, customize);
